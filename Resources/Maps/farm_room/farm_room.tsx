5af736c51908d29e404bb6cf6606cea8ab7905fd
YoumiaHina <1545629389@qq.com>
2025-12-04 19:04:04 +0800

    新增room地图

---
+++ Resources/Maps/farm_room/farm_room.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="farm_room" tilewidth="8" tileheight="8" tilecount="1056" columns="24">
+ <image source="farm_room.png" width="192" height="352"/>
+</tileset>
